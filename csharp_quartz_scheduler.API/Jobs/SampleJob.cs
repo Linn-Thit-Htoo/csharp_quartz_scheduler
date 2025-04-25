34162812cbf4ebc6c5e6d630588572b1bf1243c9
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-25 08:58:38 +0630

    -

--- csharp_quartz_scheduler.API/Jobs/SampleJob.cs
+++ csharp_quartz_scheduler.API/Jobs/SampleJob.cs
@@ -1,14 +1,13 @@
 ﻿using Quartz;
 
-namespace csharp_quartz_scheduler.API.Jobs
+namespace csharp_quartz_scheduler.API.Jobs;
+
+[DisallowConcurrentExecution]
+public class SampleJob : IJob
 {
-    [DisallowConcurrentExecution]
-    public class SampleJob : IJob
+    public Task Execute(IJobExecutionContext context)
     {
-        public Task Execute(IJobExecutionContext context)
-        {
-            Console.WriteLine("Job is executed.");
-            return Task.CompletedTask;
-        }
+        Console.WriteLine("Job is executed.");
+        return Task.CompletedTask;
     }
 }
