9cf1055e32fc8b81d4b1fbb841e682671635c7c6
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-25 08:58:23 +0630

    -

--- csharp_quartz_scheduler.API/Extensions/DependencyInjectionExtensions.cs
+++ csharp_quartz_scheduler.API/Extensions/DependencyInjectionExtensions.cs
@@ -5,26 +5,31 @@ namespace csharp_quartz_scheduler.API.Extensions;
 
 public static class DependencyInjectionExtensions
 {
-    public static IServiceCollection AddDependencies(this IServiceCollection services, WebApplicationBuilder builder)
+    public static IServiceCollection AddDependencies(
+        this IServiceCollection services,
+        WebApplicationBuilder builder
+    )
     {
         builder.Services.AddQuartz(q =>
         {
             var jobKey = new JobKey("SampleJob");
             q.AddJob<SampleJob>(opts => opts.WithIdentity(jobKey));
-            q.AddTrigger(opts => opts
-                 .ForJob(jobKey)
-                 .WithIdentity("SampleJob_Trigger")
-                 .WithCronSchedule("0/5 * * * * ?"));
+            q.AddTrigger(opts =>
+                opts.ForJob(jobKey)
+                    .WithIdentity("SampleJob_Trigger")
+                    .WithCronSchedule("0/5 * * * * ?")
+            );
         });
 
         builder.Services.AddQuartz(q =>
         {
             var jobKey = new JobKey("ReportJob");
             q.AddJob<ReportingJob>(opts => opts.WithIdentity(jobKey));
-            q.AddTrigger(opts => opts
-                 .ForJob(jobKey)
-                 .WithIdentity("ReportJob_Trigger")
-                 .WithCronSchedule("0/5 * * * * ?"));
+            q.AddTrigger(opts =>
+                opts.ForJob(jobKey)
+                    .WithIdentity("ReportJob_Trigger")
+                    .WithCronSchedule("0/5 * * * * ?")
+            );
         });
 
         builder.Services.AddQuartzHostedService(opt =>
