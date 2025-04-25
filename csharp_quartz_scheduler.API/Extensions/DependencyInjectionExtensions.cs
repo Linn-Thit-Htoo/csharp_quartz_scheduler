c33bd184d68337be7a552b76888664e1a0190c97
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-25 08:58:19 +0630

    -

--- csharp_quartz_scheduler.API/Extensions/DependencyInjectionExtensions.cs
+++ csharp_quartz_scheduler.API/Extensions/DependencyInjectionExtensions.cs
@@ -1,43 +1,42 @@
 ﻿using csharp_quartz_scheduler.API.Jobs;
 using Quartz;
 
-namespace csharp_quartz_scheduler.API.Extensions
+namespace csharp_quartz_scheduler.API.Extensions;
+
+public static class DependencyInjectionExtensions
 {
-    public static class DependencyInjectionExtensions
+    public static IServiceCollection AddDependencies(this IServiceCollection services, WebApplicationBuilder builder)
     {
-        public static IServiceCollection AddDependencies(this IServiceCollection services, WebApplicationBuilder builder)
+        builder.Services.AddQuartz(q =>
         {
-            builder.Services.AddQuartz(q =>
-            {
-                var jobKey = new JobKey("SampleJob");
-                q.AddJob<SampleJob>(opts => opts.WithIdentity(jobKey));
-                q.AddTrigger(opts => opts
-                     .ForJob(jobKey)
-                     .WithIdentity("SampleJob_Trigger")
-                     .WithCronSchedule("0/5 * * * * ?"));
-            });
+            var jobKey = new JobKey("SampleJob");
+            q.AddJob<SampleJob>(opts => opts.WithIdentity(jobKey));
+            q.AddTrigger(opts => opts
+                 .ForJob(jobKey)
+                 .WithIdentity("SampleJob_Trigger")
+                 .WithCronSchedule("0/5 * * * * ?"));
+        });
 
-            builder.Services.AddQuartz(q =>
-            {
-                var jobKey = new JobKey("ReportJob");
-                q.AddJob<ReportingJob>(opts => opts.WithIdentity(jobKey));
-                q.AddTrigger(opts => opts
-                     .ForJob(jobKey)
-                     .WithIdentity("ReportJob_Trigger")
-                     .WithCronSchedule("0/5 * * * * ?"));
-            });
+        builder.Services.AddQuartz(q =>
+        {
+            var jobKey = new JobKey("ReportJob");
+            q.AddJob<ReportingJob>(opts => opts.WithIdentity(jobKey));
+            q.AddTrigger(opts => opts
+                 .ForJob(jobKey)
+                 .WithIdentity("ReportJob_Trigger")
+                 .WithCronSchedule("0/5 * * * * ?"));
+        });
 
-            builder.Services.AddQuartzHostedService(opt =>
-            {
-                opt.WaitForJobsToComplete = true;
-            });
+        builder.Services.AddQuartzHostedService(opt =>
+        {
+            opt.WaitForJobsToComplete = true;
+        });
 
-            builder.Services.AddControllers();
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddHealthChecks();
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddHealthChecks();
 
-            return services;
-        }
+        return services;
     }
 }
